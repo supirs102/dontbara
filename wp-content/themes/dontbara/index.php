5414657e40c05ade4f4459f56a8de6261c3b4ddd
じゅえり <supirs102@gmail.com>
2020-03-09 18:49:19 +0900

    左右の動く背景準備

--- wp-content/themes/dontbara/index.php
+++ wp-content/themes/dontbara/index.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 <div id="wrapper">
-<header></header>
-<footer></footer>
+	<p>中身</p>
 </div><!-- #wrapper -->
+<div id="effect_content" class="clearfix">
+	<div id="effect_contents01"></div>
+	<div id="effect_contents02"></div>
+</div>
 <?php get_footer(); ?>
