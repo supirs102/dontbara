841ab135cb3ac17ddf5441dc748e3a2ffeae089f
じゅえり <supirs102@gmail.com>
2020-03-28 03:37:51 +0900

    ハンバーガーメニュー製作中

--- wp-content/themes/dontbara/index.php
+++ wp-content/themes/dontbara/index.php
@@ -2,6 +2,36 @@
 <div id="wrapper">
 	<p>中身</p>
 </div><!-- #wrapper -->
+
+<div id="nav-toggle">
+    <div>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</div>
+<div id="gloval-nav">
+    <nav>
+        <ul id="navigation">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Works</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <div id="nav-bg-wrap">
+        <div class="nav-bg home"></div>
+        <div class="nav-bg news"></div>
+        <div class="nav-bg about"></div>
+        <div class="nav-bg works"></div>
+        <div class="nav-bg contact"></div>
+      </div>
+</div>
+<div id="container">
+  <h1>Full Screen Hamburger Menu 02</h1>
+</div>
+
 <div id="effect_content" class="clearfix">
 	<div id="effect_contents01"></div>
 	<div id="effect_contents02"></div>
--- wp-content/themes/dontbara/js/main.js
+++ wp-content/themes/dontbara/js/main.js
@@ -1,3 +1,20 @@
 $(function(){
 	
 })
+
+    $(function () {
+        $('#nav-toggle').on('click', function() {
+            $('body').toggleClass('open');
+        });
+        $('#navigation li').on({
+            'mouseenter' : function(){
+                var index = $(this).index();
+                $('#nav-bg-wrap .nav-bg').eq(index).addClass('active');
+                
+            },
+            'mouseleave' : function(){
+                var index = $(this).index();
+                $('#nav-bg-wrap .nav-bg').eq(index).removeClass('active');
+            }
+        });
+    });
