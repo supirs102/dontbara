0bf54f45ca24332268b3982ef809875ac552b4c8
じゅえり <supirs102@gmail.com>
2020-03-28 03:38:13 +0900

    ハンバーガーメニュー製作中

--- wp-content/themes/dontbara/index.php
+++ wp-content/themes/dontbara/index.php
@@ -4,29 +4,38 @@
 </div><!-- #wrapper -->
 
 <div id="nav-toggle">
-    <div>
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
+    MENU
 </div>
-<div id="gloval-nav">
+<div id="gloval-nav" class="gloval-div">
     <nav>
-        <ul id="navigation">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Works</a></li>
-            <li><a href="#">Contact</a></li>
+        <ul id="navigation01">
+            <li><a href="#">I WANTED IS KICK</a></li>
+            <li><a href="#">GRAND MENU</a></li>
+            <li><a href="#">ONLINE STORE</a></li>
+            <li><a href="#">INFOMATION</a></li>
+            <li><a href="#">CONTACT</a></li>
         </ul>
     </nav>
-    <div id="nav-bg-wrap">
-        <div class="nav-bg home"></div>
-        <div class="nav-bg news"></div>
-        <div class="nav-bg about"></div>
-        <div class="nav-bg works"></div>
-        <div class="nav-bg contact"></div>
-      </div>
+    <nav>
+        <ul id="navigation02">
+            <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/tel.png"></a></li>
+            <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/tel.png"></a></li>
+        </ul>
+    </nav>
+    <nav>
+        <ul id="navigation03">
+            <li><a href="#">FACEBOOK</a></li>
+            <li><a href="#">INSTAGRAM</a></li>
+            <li><a href="#">YOUTUBE</a></li>
+        </ul>
+    </nav>
+</div>
+<div id="gloval-close" class="gloval-div">
+    <nav>
+        <ul id="navigation-close">
+            <li><a href="#">Close</a></li>
+        </ul>
+    </nav>
 </div>
 <div id="container">
   <h1>Full Screen Hamburger Menu 02</h1>
--- wp-content/themes/dontbara/js/main.js
+++ wp-content/themes/dontbara/js/main.js
@@ -5,16 +5,13 @@ $(function(){
     $(function () {
         $('#nav-toggle').on('click', function() {
             $('body').toggleClass('open');
-        });
-        $('#navigation li').on({
-            'mouseenter' : function(){
-                var index = $(this).index();
-                $('#nav-bg-wrap .nav-bg').eq(index).addClass('active');
-                
-            },
-            'mouseleave' : function(){
-                var index = $(this).index();
-                $('#nav-bg-wrap .nav-bg').eq(index).removeClass('active');
+            $('#nav-toggle').toggleClass('open');
+            if (document.getElementById("nav-toggle").classList.contains('open')) {
+                var elem = document.getElementById("nav-toggle");
+                elem.innerHTML = "Close";
+            } else {
+                var elem = document.getElementById("nav-toggle");
+                elem.innerHTML = "MENU";
             }
         });
     });
